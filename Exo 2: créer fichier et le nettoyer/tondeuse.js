908d9702a5cbecc2345a8876d7f0214a77fe20c3
Rachelle Cansier <chelxie98@hotmail.com>
2023-02-26 20:51:24 +0100

    Exercice 2 terminé

--- Exo 2: créer fichier et le nettoyer/tondeuse.js
+++ Exo 2: créer fichier et le nettoyer/tondeuse.js
@@ -1,27 +1,7 @@
 const fs = require('fs')
 
 const data = [
-  { id: 1, nom: 'Tondeuse 1', puissance: '10kw', autonomie: '2h', energie: 'A' },
-  { id: 1, nom: 'Tondeuse 2', puissance: '10kw', autonomie: '2h', energie: 'A' },
-  { id: 1, nom: 'Tondeuse 3', puissance: '10kw', autonomie: '2h', energie: 'A' },
-  { id: 1, nom: 'Tondeuse 4', puissance: '10kw', autonomie: '2h', energie: 'A' },
-  { id: 1, nom: 'Tondeuse 5', puissance: '10kw', autonomie: '2h', energie: 'A' },
-  { id: 1, nom: 'Tondeuse 6', puissance: '10kw', autonomie: '2h', energie: 'A' },
-  { id: 1, nom: 'Tondeuse 7', puissance: '10kw', autonomie: '2h', energie: 'A' },
-  { id: 1, nom: 'Tondeuse 8', puissance: '10kw', autonomie: '2h', energie: 'A' },
-  { id: 1, nom: 'Tondeuse 9', puissance: '10kw', autonomie: '2h', energie: 'A' },
-  { id: 1, nom: 'Tondeuse 10', puissance: '10kw', autonomie: '2h', energie: 'A' },
-  { id: 1, nom: 'Tondeuse 11', puissance: '10kw', autonomie: '2h', energie: 'A' },
-  { id: 1, nom: 'Tondeuse 12', puissance: '10kw', autonomie: '2h', energie: 'A' },
-  { id: 1, nom: 'Tondeuse 13', puissance: '10kw', autonomie: '2h', energie: 'A' },
-  { id: 1, nom: 'Tondeuse 14', puissance: '10kw', autonomie: '2h', energie: 'A' },
-  { id: 1, nom: 'Tondeuse 15', puissance: '10kw', autonomie: '2h', energie: 'A' },
-  { id: 1, nom: 'Tondeuse 16', puissance: '10kw', autonomie: '2h', energie: 'A' },
-  { id: 1, nom: 'Tondeuse 17', puissance: '10kw', autonomie: '2h', energie: 'A' },
-  { id: 1, nom: 'Tondeuse 18', puissance: '10kw', autonomie: '2h', energie: 'A' },
-  { id: 1, nom: 'Tondeuse 19', puissance: '10kw', autonomie: '2h', energie: 'A' },
-
-
+  { id: 1, nom: 'Tondeuse 1', puissance: '10kw', autonomie: '2h', energie: 'A' }
 ]
 
 const writeStream = fs.createWriteStream('tondeuse.csv')
